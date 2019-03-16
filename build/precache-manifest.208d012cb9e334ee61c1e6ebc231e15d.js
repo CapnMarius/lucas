8d2c4e1a627ad4c999dabd1300aff4bbfcbf4eae
Lucas Bos <lcssuper@gmail.com>
2019-03-16 13:07:19 +0100

    Build

---
+++ build/precache-manifest.208d012cb9e334ee61c1e6ebc231e15d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55e8939e66ab5f0ffd57",
+    "url": "/static/js/main.55e8939e.chunk.js"
+  },
+  {
+    "revision": "4f3e25d3bb967b6798e5",
+    "url": "/static/js/1.4f3e25d3.chunk.js"
+  },
+  {
+    "revision": "55e8939e66ab5f0ffd57",
+    "url": "/static/css/main.b49df995.chunk.css"
+  },
+  {
+    "revision": "615d740d83f37842d914abcea45a0475",
+    "url": "/index.html"
+  }
+];
