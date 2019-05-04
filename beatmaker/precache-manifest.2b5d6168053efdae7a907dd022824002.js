6b39282e3cf5cb29c2663bbcc189174e75a05e49
Lucas Bos <lcssuper@gmail.com>
2019-05-04 16:35:39 +0200

    Added beatmaker

---
+++ beatmaker/precache-manifest.2b5d6168053efdae7a907dd022824002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24b1fa2c218f86149b8e",
+    "url": "/static/js/main.b344bb29.chunk.js"
+  },
+  {
+    "revision": "c5d03c9e654e98ff9a66",
+    "url": "/static/js/2.d68254ee.chunk.js"
+  },
+  {
+    "revision": "24b1fa2c218f86149b8e",
+    "url": "/static/css/main.8b727c25.chunk.css"
+  },
+  {
+    "revision": "f7d26dc058bf7071fb850ee4794bc67e",
+    "url": "/index.html"
+  }
+];
