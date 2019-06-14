570830a63b5c30455840f7466474c21719f3037b
Lucas Bos <lcssuper@gmail.com>
2019-06-14 12:07:12 +0200

    Updated portfolio

---
+++ build/precache-manifest.6ec060df01d1865ce9591606f6c08150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88ebf5f4bde0a6f86954",
+    "url": "/static/js/main.88ebf5f4.chunk.js"
+  },
+  {
+    "revision": "76f2d28d423e01d7e783",
+    "url": "/static/js/1.76f2d28d.chunk.js"
+  },
+  {
+    "revision": "88ebf5f4bde0a6f86954",
+    "url": "/static/css/main.06313504.chunk.css"
+  },
+  {
+    "revision": "1b4634928a8d6a785ed9ed46979f2505",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ab628c7bf46b0fa5f674c828e3a676b.js"
+  "/precache-manifest.6ec060df01d1865ce9591606f6c08150.js"
 );
 
 workbox.clientsClaim();
