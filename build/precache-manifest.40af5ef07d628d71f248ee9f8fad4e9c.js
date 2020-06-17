223963af84b75120f9734b556b7c1e3d0b745e26
Lucas Bos <lucas@minox.nl>
2020-06-17 20:58:17 +0200

    Updated portfolio site

---
+++ build/precache-manifest.40af5ef07d628d71f248ee9f8fad4e9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6935f6458ab0f1488907",
+    "url": "/static/css/main.717dcf0f.chunk.css"
+  },
+  {
+    "revision": "6935f6458ab0f1488907",
+    "url": "/static/js/main.6935f645.chunk.js"
+  },
+  {
+    "revision": "a9130fcdfcf86cb085bd",
+    "url": "/static/js/1.a9130fcd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3313191389b382aed7cda3389f1e6384",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ec060df01d1865ce9591606f6c08150.js"
+  "/precache-manifest.40af5ef07d628d71f248ee9f8fad4e9c.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
