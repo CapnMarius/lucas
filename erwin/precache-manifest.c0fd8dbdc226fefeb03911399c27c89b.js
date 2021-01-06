1de12ceec3517662ac65682abbcbe60d03cd4c03
Lucas Bos <lucas@minox.nl>
2021-01-06 20:51:22 +0100

    Added erwin

---
+++ erwin/precache-manifest.c0fd8dbdc226fefeb03911399c27c89b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ebd1faa2658637c17634f7c460b0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fecb6a745a7992f042f",
+    "url": "/static/css/main.482c8748.chunk.css"
+  },
+  {
+    "revision": "1025bdb6f31a2be84b0e",
+    "url": "/static/js/2.0b878201.chunk.js"
+  },
+  {
+    "revision": "8fecb6a745a7992f042f",
+    "url": "/static/js/main.ecf7ed0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
