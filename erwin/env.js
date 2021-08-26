59df10d5339df5e3432721303bce8d6a9e5bebb2
Lucas Bos <lucas@minox.nl>
2021-08-26 10:15:59 +0200

    Removed Spotify credentials from env

--- erwin/env.js
+++ erwin/env.js
@@ -6,7 +6,5 @@ window.env = {
   "REDIRECT_PRD": "https://lucasbos.dev/erwin",
   "BACKEND_BASE_DEV": "localhost:5000",
   "BACKEND_BASE_PRD": "api.lucasbos.dev",
-  "SPOTIFY_CLIENT_ID": "8a534d9a1d1742dfacf9172ee89c29cb",
-  "SPOTIFY_CLIENT_SECRET": "2087c55c839749eb9dc694b7f7b3c625",
   "BUCKET_NAME": "playlist-memories"
 };
